2fe9419169f3b060efd67ce306fc4b1714b993b4
Michael Jay <xuchuaner@qq.com>
2016-09-14 20:37:31 +0000

    remove cp.sh

--- cp.sh
+++
@@ -1,9 +0,0 @@
-#########################################################################
-# File Name: cp.sh
-# Author: Michael Jay
-# mail: michaelxu2016@yahoo.com
-# Created Time: 2016年08月16日 星期二 16时22分02秒
-# Function: 
-#########################################################################
-#!/bin/bash
-	 cp *.ko  ~/mynfs/rootfs/kotest/ -f
